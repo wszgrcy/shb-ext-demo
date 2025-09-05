5ce39809ba5fc66d50177b5fa0364f9f8a1fa15e
chen yang <wszgrcy@gmail.com>
2025-09-05 13:12:09 +0800

    auto publish

--- script/auto-publish.ts
+++ script/auto-publish.ts
@@ -1,25 +1,19 @@
-import { version } from '../assets/package.json';
+import { version, name } from '../assets/package.json';
 import * as core from '@actions/core';
 export async function main() {
-  console.log('xxxxxxxx');
   let { $ } = await import('execa');
   let result2 = await $`git tag`;
-  console.log(result2.stdout);
   let exist = result2.stdout
     .trim()
     .split(/\r\n|\n/)
     .some((item) => item === version);
   if (exist) {
-    console.log('相同');
     return;
   }
-  console.log(version);
-  // await $({ stdio: 'inherit' })`npm run local-publish`;
-  // await $({ stdio: 'inherit' })`git tag ${version}`;
-  // await $({ stdio: 'inherit' })`git push origin ${version}`;
-  process.env['VERSION'] = version;
-  console.log(process.env);
+  await $({ stdio: 'inherit' })`npm run local-publish`;
+  await $({ stdio: 'inherit' })`git tag ${version}`;
+  await $({ stdio: 'inherit' })`git push origin ${version}`;
   core.setOutput('VERSION', version);
-  return { version };
+  core.setOutput('NAME', name);
 }
 main();

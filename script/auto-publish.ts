61650d807d66901fc059783c3844e058c54e0450
chen yang <wszgrcy@gmail.com>
2025-09-05 13:06:40 +0800

    1

--- script/auto-publish.ts
+++ script/auto-publish.ts
@@ -1,6 +1,7 @@
 import { version } from '../assets/package.json';
+import * as core from '@actions/core';
 export async function main() {
-  console.log('xxxxxxxx')
+  console.log('xxxxxxxx');
   let { $ } = await import('execa');
   let result2 = await $`git tag`;
   console.log(result2.stdout);
@@ -12,12 +13,13 @@ export async function main() {
     console.log('相同');
     return;
   }
-  console.log(version)
+  console.log(version);
   // await $({ stdio: 'inherit' })`npm run local-publish`;
   // await $({ stdio: 'inherit' })`git tag ${version}`;
   // await $({ stdio: 'inherit' })`git push origin ${version}`;
   process.env['VERSION'] = version;
   console.log(process.env);
+  core.setOutput('VERSION', version);
   return { version };
 }
 main();
